eece415e39d3f6588a803aa91c426dd985548631
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-09-10 13:39:51 -0500

    :sparkles: build config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { HistoryModule } from './history/history.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FavoritesModule } from './favorites/favorites.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/torre-front'),
+    ConfigModule.forRoot({ load: [EnvConfiguration] }),
+    MongooseModule.forRoot(process.env.MONGODB),
     FavoritesModule,
     HistoryModule,
   ],
