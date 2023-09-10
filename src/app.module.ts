a14c788bebcb338a99b76791e271262a8009a5c2
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2023-09-10 08:47:48 -0500

    :sparkles: mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { HistoryModule } from './history/history.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule, HistoryModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/torre-front'),
+    UserModule,
+    HistoryModule,
+  ],
   controllers: [],
   providers: [],
 })
